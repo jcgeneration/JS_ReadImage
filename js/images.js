fa3e7cb6aa711fd2c9f5e9a1c89723f5387b48a1
jc <juan.valencia@idr.edu.mx>
2022-03-24 15:47:17 -0600

    Versión inicial

---
+++ js/images.js
@@ -0,0 +1,28 @@
+	let fileImage = document.getElementById('fileImage');
+	let btnFake = document.getElementById('btnFake');
+	let imageFile = document.getElementById('imageFile');
+
+	btnFake.addEventListener('click', function(){
+		fileImage.click();
+    });
+    fileImage.addEventListener('change', function(){
+        previewFile('imageFile', 'fileImage', 'inputFile' )
+        //previewFile(img, input type file ,textArea);
+    });
+
+		// previewFile(img, num, input)
+		function previewFile(img, inputFile, input) {
+			
+			var preview = document.getElementById(img);
+			var file    = document.getElementById(inputFile).files[0];
+			var reader  = new FileReader();
+
+			reader.addEventListener("load", function () {
+				document.getElementById(input).value = reader.result;
+		  		preview.src = reader.result;
+		  	}, false);
+			
+		  	if (file) {
+		    	reader.readAsDataURL(file);
+		  	}// file
+		}// previewFile 
