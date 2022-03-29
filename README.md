0929129960ad773cda2f9074c5b92aeab49c3086
jc <juan.valencia@idr.edu.mx>
2022-03-28 20:23:30 -0600

    Se agregan las imágenes al LocalStorage y un Select para visualizar las imágenes previamente cargadas

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JS_ReadImage
 Leer una imagen convertir a Base64 y hacer preview, con JavaScript
+
+## Select con visualización de las imágenes guardadas en LocalStorage
--- js/images.js
+++ js/images.js
@@ -1,6 +1,35 @@
 	let fileImage = document.getElementById('fileImage');
 	let btnFake = document.getElementById('btnFake');
 	let imageFile = document.getElementById('imageFile');
+	let selectImages = document.getElementById('selectImages');
+	let store = {'names':[], 'data': []};
+
+	window.addEventListener('load', function(e){
+		console.log("store.data:" + store.data);
+		console.log(localStorage.getItem("imgsData"));
+		if ( JSON.parse(localStorage.getItem("imgsData"))!=null)
+			store = JSON.parse(localStorage.getItem("imgsData"));
+		
+		fillSelect();
+	});
+
+	function fillSelect(){
+		if (store.data.length>0) {
+			selectImages.options.length = 0; // clear Select
+			let optiontmp = document.createElement("option");
+				optiontmp.text = "Selecciona una imagen para visualizar:"
+				optiontmp.value = -1;
+				selectImages.add(optiontmp); 
+			for (let i=0; i < store.data.length; i ++){
+				let option = document.createElement("option");
+				option.text = store.names[i];
+				option.value = i;
+				selectImages.add(option); 
+			}//for i
+		}//if store != null
+		console.log(store);
+	}//fillSelect
+
 
 	btnFake.addEventListener('click', function(){
 		fileImage.click();
@@ -12,17 +41,27 @@
 
 		//previewFile(id imagen, input type file , textArea);
 		function previewFile(img, inputFile, input) {
-			
 			var preview = document.getElementById(img);
 			var file    = document.getElementById(inputFile).files[0];
 			var reader  = new FileReader();
 
 			reader.addEventListener("load", function () {
+				console.log(store.data.length);
+				store.data[store.data.length] = reader.result;
+				store.names[store.names.length] =file.name;
+				console.table(store);
 				document.getElementById(input).value = reader.result;
 		  		preview.src = reader.result;
+				localStorage.setItem("imgsData", JSON.stringify(store));
+				fillSelect();
 		  	}, false);
 			
 		  	if (file) {
 		    	reader.readAsDataURL(file);
 		  	}// file
 		}// previewFile 
+
+
+		selectImages.addEventListener("change", function(){
+			imageFile.src = store.data[selectImages.options[selectImages.selectedIndex].value];
+		});
